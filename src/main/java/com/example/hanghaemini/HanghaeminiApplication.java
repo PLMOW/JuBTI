8365a1d3bef3c255903514f536cc3c4abae30bcd
Galmaeki <122267336+Galmaeki@users.noreply.github.com>
2023-02-24 11:54:00 +0900

    Add files via upload
    TestUpload

---
+++ src/main/java/com/example/hanghaemini/HanghaeminiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeminiApplication.class, args);
+    }
+
+}
