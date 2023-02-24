7d6d6ac153105b12655542e7a02e3ef3b0d3e476
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-24 22:39:35 +0900

    feat: RecipeRequestDto 생성

---
+++ src/main/java/com/example/jubtibe/domain/recipe/dto/RecipeRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.jubtibe.domain.recipe.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RecipeRequestDto {
+    //    작성자 박성민
+    //    이미지 파일 받아오기
+    private String title;
+    private Enum mbti;
+    private String material;
+    private String content;
+}
