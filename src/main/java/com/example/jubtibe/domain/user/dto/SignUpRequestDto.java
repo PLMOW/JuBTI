25d56baf1e2a01a1433b20e4da6f7ecc635a9caa
choincnp <whdudwns7777@gmail.com>
2023-02-25 02:28:57 +0900

    feat: Add some properties

--- src/main/java/com/example/jubtibe/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/example/jubtibe/domain/user/dto/SignUpRequestDto.java
@@ -1,14 +1,16 @@
 package com.example.jubtibe.domain.user.dto;
 
 import com.example.jubtibe.domain.user.entity.UserMbti;
+import lombok.Getter;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.validation.constraints.Pattern;
 
 
+@Getter
 public class SignUpRequestDto {
-    //작성자 권재현
+    //작성자 권재현 , 조영준
     private String username;
     @Pattern(regexp = "[a-z0-9]{4,10}")
     private String nickname;
@@ -16,4 +18,8 @@ public class SignUpRequestDto {
     private String password;
     @Enumerated(value = EnumType.STRING)
     private UserMbti mbti;
+
+    private boolean admin = false;
+
+    private String adminToken = "";
 }
