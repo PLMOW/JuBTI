638c6b2b0ffe352931f1d16b7548865ed8d1352e
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-25 00:13:25 +0900

    feat: RecipeResponseDto 생성

---
+++ src/main/java/com/example/jubtibe/domain/recipe/dto/RecipeResponseDto.java
@@ -0,0 +1,33 @@
+package com.example.jubtibe.domain.recipe.dto;
+
+import com.example.jubtibe.domain.recipe.entity.Recipe;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class RecipeResponseDto {
+    //    작성자 박성민
+    //    이미지파일 받아오기
+    private String nickname;
+    private String title;
+    private String material;
+    private String content;
+    private Enum mbti;
+    private int like;
+    private List<Comments> comments;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public RecipeResponseDto(Recipe recipe, List<Comments> comments) {
+        this.nickname = getNickname();
+        this.title = getTitle();
+        this.material = getMaterial();
+        this.content = getContent();
+        this.mbti = getMbti();
+        this.like = getLike();
+        this.comments = comments;
+    }
+}
