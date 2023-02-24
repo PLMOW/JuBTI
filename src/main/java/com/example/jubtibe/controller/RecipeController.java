bf372152dca068b8bea875aef8ecad2ad245ac53
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-25 00:10:58 +0900

    fix: 반환타입 수정 및 유효성 검사 추가

--- src/main/java/com/example/jubtibe/controller/RecipeController.java
+++ src/main/java/com/example/jubtibe/controller/RecipeController.java
@@ -1,14 +1,13 @@
 package com.example.jubtibe.controller;
 
 import com.example.jubtibe.domain.recipe.dto.RecipeRequestDto;
+import com.example.jubtibe.domain.recipe.dto.RecipeResponseDto;
 import com.example.jubtibe.domain.recipe.service.RecipeService;
 import com.example.jubtibe.dto.StatusResponseDto;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/api")
@@ -19,27 +18,27 @@ public class RecipeController {
     private final RecipeService recipeService;
 
     @PostMapping("/recipe")
-    public StatusResponseDto createRecipe(@RequestBody RecipeRequestDto requestDto){
+    public StatusResponseDto createRecipe(@Valid @RequestBody RecipeRequestDto requestDto){
         return recipeService.createRecipe(requestDto);
     }
 
     @GetMapping("/recipe")
-    public List<RecipeRequestDto> getRecipes(){
+    public List<RecipeResponseDto> getRecipes(){
         return recipeService.getRecipes();
     }
 
     @GetMapping("/recipe/{id}")
-    public StatusResponseDto getRecipe(@PathVariable Long id){
+    public RecipeResponseDto getRecipe(@PathVariable Long id){
         return recipeService.getRecipe(id);
     }
 
     @PutMapping("/recipe/{id}")
-    public StatusResponseDto updateRecipe(@PathVariable Long id, @RequestBody RecipeRequestDto requestDto){
+    public StatusResponseDto updateRecipe(@Valid @PathVariable Long id, @RequestBody RecipeRequestDto requestDto){
         return recipeService.updateRecipe(id, requestDto);
     }
 
     @DeleteMapping("/recipe/{id}")
-    public StatusResponseDto delete(@PathVariable Long id, @RequestBody RecipeRequestDto requestDto){
-        return recipeService.deleteRecipe(id, requestDto);
+    public StatusResponseDto deleteRecipe(@PathVariable Long id){
+        return recipeService.deleteRecipe(id);
     }
 }
