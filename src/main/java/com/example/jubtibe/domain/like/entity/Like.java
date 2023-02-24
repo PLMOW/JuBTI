b11b368fb7b2e0a949fa0c1e999bd5fc2f59eaf4
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-24 22:55:26 +0900

    feat: Like entity 생성

---
+++ src/main/java/com/example/jubtibe/domain/like/entity/Like.java
@@ -0,0 +1,23 @@
+package com.example.jubtibe.domain.like.entity;
+
+import com.example.jubtibe.domain.recipe.entity.Recipe;
+import com.example.jubtibe.domain.user.entity.User;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+public class Like {
+    //작성자 박성민, 권재현
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="username")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="recipeId")
+    private Recipe recipe;
+}
