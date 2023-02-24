17715edd92d11e3052e14ad1d963eda80fd3988d
Galmaeki <kjh09800@gmail.com>
2023-02-24 20:04:33 +0900

    refactor: MBTI 이넘클래스 작성

--- src/main/java/com/example/jubtibe/domain/user/entity/User.java
+++ src/main/java/com/example/jubtibe/domain/user/entity/User.java
@@ -1,7 +1,27 @@
 package com.example.jubtibe.domain.user.entity;
 
-import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import javax.persistence.*;
+
+@Getter
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Mbti mbti;
 }
