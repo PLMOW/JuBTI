2e3811f2e402927faff86572b72bf3a3e0fcf473
Galmaeki <kjh09800@gmail.com>
2023-02-25 00:10:48 +0900

    refactor: 어노테이션 변경, 사용안하는 import제거

--- src/main/java/com/example/jubtibe/domain/comments/dto/CommentsRequestDto.java
+++ src/main/java/com/example/jubtibe/domain/comments/dto/CommentsRequestDto.java
@@ -1,12 +1,11 @@
 package com.example.jubtibe.domain.comments.dto;
 
-import com.sun.istack.NotNull;
+
 import lombok.Getter;
 
 @Getter
 public class CommentsRequestDto {
     //작성자 권재현
-    @NotNull
     private String comments;
 
 }
