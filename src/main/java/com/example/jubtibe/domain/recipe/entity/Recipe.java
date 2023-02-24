ae546aef0b56819088782b1e25f5ac40048d43a6
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-25 00:14:10 +0900

    feat: Recipe생성

---
+++ src/main/java/com/example/jubtibe/domain/recipe/entity/Recipe.java
@@ -0,0 +1,60 @@
+package com.example.jubtibe.domain.recipe.entity;
+
+import com.example.jubtibe.domain.like.entity.Like;
+import com.example.jubtibe.domain.recipe.dto.RecipeRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Recipe {
+    //    작성자 박성민
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Enum mbti;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String material;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime modifiedAt;
+
+    @OneToMany
+    @JoinColumn(name = "commentsId")
+    private Comments comments;
+
+    @OneToMany
+    @JoinColumn(name = "likeId")
+    private Like like;
+
+    public Recipe(RecipeRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.mbti = requestDto.getMbti();
+        this.content = requestDto.getContent();
+        this.material = requestDto.getMaterial();
+    }
+
+    public void update(RecipeRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.mbti = requestDto.getMbti();
+        this.content = requestDto.getContent();
+        this.material = requestDto.getMaterial();
+    }
+}
