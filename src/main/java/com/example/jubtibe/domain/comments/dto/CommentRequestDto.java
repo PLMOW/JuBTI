788cd0e1436dff2f6ac971b1192ae7ddca294432
Galmaeki <kjh09800@gmail.com>
2023-02-24 23:24:48 +0900

    refactor: Comments 패키지명 변경 및 클래스 명 변경

---
+++ src/main/java/com/example/jubtibe/domain/comments/dto/CommentRequestDto.java
@@ -0,0 +1,12 @@
+package com.example.jubtibe.domain.comments.dto;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+
+@Getter
+public class CommentsRequestDto {
+    //작성자 권재현
+    @NotNull
+    private String comments;
+
+}
