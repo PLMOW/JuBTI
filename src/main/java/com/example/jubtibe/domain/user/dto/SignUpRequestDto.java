14c6bc7c29218fd27859581e19a10c8fdd642f94
Galmaeki <kjh09800@gmail.com>
2023-02-25 00:11:17 +0900

    refactor: 정규표현식 추가, 유효성검증 어노테이션 추가

--- src/main/java/com/example/jubtibe/domain/user/dto/SignUpRequestDto.java
+++ src/main/java/com/example/jubtibe/domain/user/dto/SignUpRequestDto.java
@@ -2,10 +2,18 @@ package com.example.jubtibe.domain.user.dto;
 
 import com.example.jubtibe.domain.user.entity.UserMbti;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Pattern;
+
+
 public class SignUpRequestDto {
     //작성자 권재현
     private String username;
+    @Pattern(regexp = "[a-z0-9]{4,10}")
     private String nickname;
+    @Pattern(regexp = "[!@#$%^&*][A-Za-z0-9]{8,15}")
     private String password;
+    @Enumerated(value = EnumType.STRING)
     private UserMbti mbti;
 }
