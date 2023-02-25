92508d7c7612c526da0d5760786eff115daffcaa
Galmaeki <kjh09800@gmail.com>
2023-02-25 22:04:47 +0900

    refactor: comments > comment 리팩토링

--- src/main/java/com/example/jubtibe/controller/CommentController.java
+++ src/main/java/com/example/jubtibe/controller/CommentController.java
@@ -1,10 +1,10 @@
 package com.example.jubtibe.controller;
 
 
-import com.example.jubtibe.domain.comment.dto.CommentsRequestDto;
+import com.example.jubtibe.domain.comment.dto.CommentRequestDto;
 import com.example.jubtibe.dto.StatusResponseDto;
 import com.example.jubtibe.security.UserDetailsImpl;
-import com.example.jubtibe.service.CommentsService;
+import com.example.jubtibe.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +12,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
-public class CommentsController {
+public class CommentController {
     //작성자 권재현
-    private final CommentsService commentsService;
+    private final CommentService commentService;
 
     @PostMapping("/recipe/{id}/comment")
-    public StatusResponseDto createComments(@PathVariable Long id, @RequestBody CommentsRequestDto requestDto,@AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return commentsService.createComments(id,requestDto,userDetails.getUsername());
+    public StatusResponseDto createComments(@PathVariable Long id, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return commentService.createComments(id,requestDto,userDetails.getUsername());
     }
 
     @PutMapping("/recipe/comment/{commentId}")
-    public StatusResponseDto updateComments(@PathVariable Long commentId, @RequestBody CommentsRequestDto requestDto,@AuthenticationPrincipal UserDetailsImpl userDetails){
-        return commentsService.updateComments(commentId,requestDto,userDetails.getUsername());
+    public StatusResponseDto updateComments(@PathVariable Long commentId, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        return commentService.updateComments(commentId,requestDto,userDetails.getUsername());
     }
     @DeleteMapping("/recipe/comment/{commentId}")
     public StatusResponseDto deleteComments(@PathVariable Long commentId ,@AuthenticationPrincipal UserDetailsImpl userDetails){
-        return commentsService.deleteComments(commentId,userDetails.getUsername());
+        return commentService.deleteComments(commentId,userDetails.getUsername());
     }
 //    @PostMapping("/post") // 게시글 작성
 //    public ResponseEntity<?> postLetter(@RequestBody LetterRequestDto letterRequestDto, ){
--- src/main/java/com/example/jubtibe/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/jubtibe/domain/comment/dto/CommentRequestDto.java
@@ -4,7 +4,7 @@ package com.example.jubtibe.domain.comment.dto;
 import lombok.Getter;
 
 @Getter
-public class CommentsRequestDto {
+public class CommentRequestDto {
     //작성자 권재현
     private String comments;
 
