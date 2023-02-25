3355ba37839fecd08d0f90e702be07e758a758f3
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-25 18:25:43 +0900

    fix: 클래스 중복 수정

--- src/main/java/com/example/jubtibe/domain/recipe/dto/RecipeSearchDto.java
+++ src/main/java/com/example/jubtibe/domain/recipe/dto/RecipeSearchDto.java
@@ -7,33 +7,28 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
-public class RecipeSearchDto {
     // 작성자 박성민
 
-    @Getter
-    @NoArgsConstructor
-    public class RecipeSearchDto {
-        //    작성자 박성민
-        //    이미지파일 받아오기
-        private Long id;
-        private String nickname;
-        private String title;
-        private String material;
-        private String content;
-        private Enum userMbti;
-        private Like like;
-        private LocalDateTime createdAt;
-        private LocalDateTime modifiedAt;
+@Getter
+@NoArgsConstructor
+public class RecipeSearchDto {
+    //    작성자 박성민
+    //    이미지파일 받아오기
+    private Long id;
+    private String nickname;
+    private String title;
+    private String material;
+    private String content;
+    private Enum userMbti;
+    private Like like;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
-        public RecipeSearchDto(Recipe recipe) {
-            this.id = recipe.getRecipeId();
-            this.nickname = recipe.getUser().getNickname();
-            this.title = recipe.getTitle();
-            this.material = recipe.getMaterial();
-            this.content = recipe.getContent();
-            this.userMbti = recipe.getUserMbti();
-            this.like = recipe.getLike();
-        }
+    public RecipeSearchDto(Recipe recipe) {
+        this.id = recipe.getRecipeId();
+        this.nickname = recipe.getUser().getNickname();
+        this.title = recipe.getTitle();
+        this.userMbti = recipe.getUserMbti();
+        this.like = recipe.getLike();
     }
 }
