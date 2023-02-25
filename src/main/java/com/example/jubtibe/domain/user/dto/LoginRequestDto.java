7471b56c8adee3e3eb3214feebc9981b9310f521
Galmaeki <kjh09800@gmail.com>
2023-02-25 18:10:55 +0900

    feat: LoginRequestDto 작성

---
+++ src/main/java/com/example/jubtibe/domain/user/dto/LoginRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.jubtibe.domain.user.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequestDto {
+    //작성자 권재현
+    private String username;
+    private String password;
+}
