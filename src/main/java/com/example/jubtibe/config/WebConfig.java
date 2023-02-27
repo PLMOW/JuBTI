8e488a267506ccd301b26ad50b6035c940476dc4
choincnp <whdudwns7777@gmail.com>
2023-02-27 19:57:51 +0900

    feat: Webconfig 수정

--- src/main/java/com/example/jubtibe/config/WebConfig.java
+++ src/main/java/com/example/jubtibe/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("OPTIONS","GET","POST","PUT","PATCH","DELETE");
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("OPTIONS","GET","POST","PUT","PATCH","DELETE")
+                .maxAge(3600)
+        ;
     }
 }
