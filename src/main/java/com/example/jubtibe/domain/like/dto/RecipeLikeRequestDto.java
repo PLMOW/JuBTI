10f2185244714be83e3f178b2dafd5d183dde4cc
SeongMinnnn <122071252+SeongMinnnn@users.noreply.github.com>
2023-02-28 01:56:38 +0900

    fix: recipeId 오타 수정

--- src/main/java/com/example/jubtibe/domain/like/dto/RecipeLikeRequestDto.java
+++ src/main/java/com/example/jubtibe/domain/like/dto/RecipeLikeRequestDto.java
@@ -1,10 +1,9 @@
 package com.example.jubtibe.domain.like.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 public class RecipeLikeRequestDto {
     private Long userId;
-    private Long RecipeId;
+    private Long recipeId;
 }
