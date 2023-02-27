a01ea02f21daf1db36cb19a63d1d80525c728a0c
choincnp <whdudwns7777@gmail.com>
2023-02-27 16:39:22 +0900

    feat: application jpaAuditing 추가

--- src/main/java/com/example/jubtibe/JuBtiBeApplication.java
+++ src/main/java/com/example/jubtibe/JuBtiBeApplication.java
@@ -2,8 +2,10 @@ package com.example.jubtibe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JuBtiBeApplication {
 
     public static void main(String[] args) {
