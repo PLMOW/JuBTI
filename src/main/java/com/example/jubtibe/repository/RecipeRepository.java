3167e847207ac187d4db098e8f802bca3a626523
choincnp <whdudwns7777@gmail.com>
2023-03-02 19:26:26 +0900

    feat: repository 수정

--- src/main/java/com/example/jubtibe/repository/RecipeRepository.java
+++ src/main/java/com/example/jubtibe/repository/RecipeRepository.java
@@ -2,10 +2,12 @@ package com.example.jubtibe.repository;
 
 import com.example.jubtibe.domain.recipe.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository <Recipe, Long> {
     // 작성자 박성민
+    @Query(value = "SELECT DISTINCT r from Recipe r order by r.recipeLike.size desc")
     List<Recipe> findAllByOrderByRecipeLikeDesc();
 }
